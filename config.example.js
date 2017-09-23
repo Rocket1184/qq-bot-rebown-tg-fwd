2f804e097074192d840ada882b13b973470ff137
rocka <i@rocka.me>
2017-09-23 13:55:19 +0800

    basic functions

---
+++ config.example.js
@@ -0,0 +1,29 @@
+module.exports = {
+    server: {
+        // Visit `http://${host}:${port}${get_qr_path}` to get QQ login QR code
+        get_qr_path: '/tg-qq-fwd/qq-qr-code',
+        port: 34737
+    },
+    tg: {
+        // Telegram bot token
+        bot_token: '123456:ABCdef_ghiJKLmnOPQrstu-vw',
+        // Telegram group chat ID, can be number or @group_name
+        chat_id: '@group_name',
+        // function to transform forwarded msg, you can use markdown in output
+        // arguments:
+        // msg: { name: string; content: string; groupName: string }
+        // kwd: string, matched keyword
+        transformMsg: function (msg, kwd) {
+            return `[${msg.name}] ${msg.content.replace(kwd, `\`${kwd}\``)}`;
+        }
+    },
+    qq: {
+        // QQ group FULL name
+        group_name: '...',
+        // keywords to listen
+        listen_keywords: [
+            'fwd',
+            'test'
+        ]
+    }
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+
+const tg = require('telegraf');
+const qq = require('qq-bot-rebown');
+const config = require('./config');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'get' && ~req.url.indexOf(config.server.get_qr_path)) {
+        fs.createReadStream('/tmp/code.png').pipe(res);
+    }
+});
+
+server.listen(config.server.port);
+
+const tgBot = new tg.Telegram(config.tg.bot_token);
+
+const qqMsgHandler = new qq.MsgHandler(
+    (msg, qq) => {
+        if (~msg.groupName.indexOf(config.qq.group_name)) {
+            for (let kwd of config.qq.listen_keywords) {
+                if (~msg.content.indexOf(kwd)) {
+                    tgBot.sendMessage(
+                        config.tg.chat_id,
+                        config.tg.transformMsg(msg, kwd), {
+                            parse_mode: "Markdown"
+                        }
+                    );
+                    return;
+                }
+            }
+        }
+    },
+    'group'
+)
+
+const qqBot = new qq.QQ(qqMsgHandler).run();
